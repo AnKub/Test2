219733b0c9f7abdac24d11d276e0a31db391f1c0
AnKub <ankub3088@gmail.com>
2025-04-06 15:59:04 +0300

    faq

--- script.js
+++ script.js
@@ -117,7 +117,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
   faqButtons.forEach(button => {
     button.addEventListener('click', () => {
+      const answer = button.nextElementSibling;
       button.classList.toggle('active');
+      if (answer.style.display === 'block') {
+        answer.style.display = 'none';
+      } else {
+        answer.style.display = 'block';
+      }
     });
   });
 });
